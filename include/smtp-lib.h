3bf7319f2897cc5e70b3c94dfbef9845f9b644ed
Tomasz Pieczerak <tphaster@gmail.com>
2011-12-14 14:17:10 +0100

    Sending SMTP Replies and a few other changes
    * include/smtp-lib.h
    Added SMTP Replies codes definitions, changed HOSTNAME_MAXLEN to
    DOMAIN_MAXLEN, changed declaration of smtp_send_reply().
    
    * src/smtp-lib.c
    Changed "hostname" to "domain", except for gethostname() function
    (should be fixed later). Finished smtp_send_reply(), needs some testing.

--- include/smtp-lib.h
+++ include/smtp-lib.h
@@ -17,7 +17,29 @@
 #define NOOP    8
 #define QUIT    9
 
-#define HOSTNAME_MAXLEN     128
+/* SMTP Replies */
+#define R211     1  /* (after QUIT) closing connection */
+#define R220     2  /* (on connection start) greeting */
+#define R250     3  /* requested mail action okay, completed */
+#define R250E    4  /* same as R250, but to be continued */
+#define R251     5  /* (after RCPT) user not local; will forward to <forward-path> */
+#define R354     6  /* (after DATA) start mail input; end with <CRLF>.<CRLF> */
+#define R450     7  /* requested mail action not taken: mailbox unavailable */
+#define R451     8  /* requested action aborted: local error in processing */
+#define R452     9  /* requested action not taken: insufficient system storage */
+#define R455    10  /* server unable to accommodate parameters */
+#define R502    11  /* (after EHLO) command not implemented */
+#define R503    12  /* bad sequence of commands */
+#define R504    13  /* (after HELO/EHLO) command parameter not implemented */
+#define R550    14  /* requested action not taken: mailbox unavailable */
+#define R551    15  /* (after RCPT) user not local; please try <forward-path> */
+#define R552    16  /* requested mail action aborted: exceeded storage allocation */
+#define R553    17  /* (after RCPT) requested action not taken: mailbox name not allowed */
+#define R554    18  /* transaction failed */
+#define R555    19  /* MAIL FROM/RCPT TO parameters not recognized or not implemented */
+
+/* Constants */
+#define DOMAIN_MAXLEN       128
 #define LINE_MAXLEN         256
 #define ADDR_MAXLEN         128
 
@@ -29,5 +51,5 @@ struct mail_object {
 };
 
 int smtp_send_command (int sockfd, size_t cmd, struct mail_object *mail);
-int smtp_send_reply (int sockfd);
+int smtp_send_reply (int sockfd, size_t code, const char *msg, size_t msg_len);
 
--- src/smtp-lib.c
+++ src/smtp-lib.c
@@ -4,12 +4,6 @@
  * Author:       Tomasz Pieczerak (tphaster)
  */
 
-/* TODO
- *  smtp_send_reply()
- */
-
-#include <stdio.h>
-
 #include <string.h>
 #include <unistd.h>
 
@@ -18,7 +12,7 @@
 
 int smtp_send_command (int sockfd, size_t cmd, struct mail_object *mail)
 {
-    char hostname[HOSTNAME_MAXLEN], cmd_line[LINE_MAXLEN];
+    char domain[DOMAIN_MAXLEN], cmd_line[LINE_MAXLEN];
     size_t i;
     size_t pos = 0;
 
@@ -33,10 +27,10 @@ int smtp_send_command (int sockfd, size_t cmd, struct mail_object *mail)
             pos = 5;
 
             /* hostname */
-            bzero(hostname, sizeof hostname);
-            gethostname(hostname, sizeof hostname);
-            strncpy(cmd_line+pos, hostname, HOSTNAME_MAXLEN);
-            pos += min(HOSTNAME_MAXLEN, strlen(hostname));
+            bzero(domain, sizeof domain);
+            gethostname(domain, sizeof domain);
+            strncpy(cmd_line+pos, domain, DOMAIN_MAXLEN);
+            pos += min(DOMAIN_MAXLEN, strlen(domain));
 
             /* terminating CRLF */
             strcpy(cmd_line+pos, "\r\n");
@@ -46,7 +40,7 @@ int smtp_send_command (int sockfd, size_t cmd, struct mail_object *mail)
 
         case MAIL:
             if (NULL == mail)
-                return -1;  /* NULL pointer dereference*/
+                return -1;  /* NULL pointer dereference */
 
             /* command code */
             strcpy(cmd_line, "MAIL FROM:<");
@@ -118,6 +112,265 @@ int smtp_send_command (int sockfd, size_t cmd, struct mail_object *mail)
             return -1;  /* no such command */
             break;
     }
+
+    return 0;
+}
+
+int smtp_send_reply (int sockfd, size_t code, const char *msg, size_t msg_len)
+{
+    char domain[DOMAIN_MAXLEN], rply_line[LINE_MAXLEN];
+    size_t pos = 0;
+
+    switch (code) {
+        case R211:  /* (after QUIT) closing connection */
+            if (NULL == msg) {
+                /* reply code, text string and terminating <CRLF> */
+                strcpy(rply_line, "211 closing connection, bye\r\n");
+                Writen(sockfd, rply_line, strlen(rply_line));
+                return 0;
+            }
+            else {
+                strcpy(rply_line, "211 ");
+                /* text string is user defined */
+                break;
+            }
+
+        case R220:  /* (on connection start) greeting */
+            if (NULL == msg) {
+                /* reply code */
+                strcpy(rply_line, "220 ");
+                pos = 4;
+
+                /* hostname */
+                bzero(domain, sizeof domain);
+                gethostname(domain, sizeof domain);
+                strncpy(rply_line+pos, domain, DOMAIN_MAXLEN);
+                pos += min(DOMAIN_MAXLEN, strlen(domain));
+
+                /* text string and terminating CRLF */
+                strcpy(rply_line+pos, " Service ready\r\n");
+
+                Writen(sockfd, rply_line, strlen(rply_line));
+                return 0;
+            }
+            else {
+                strcpy(rply_line, "220 ");
+                break;
+            }
+
+        case R250:  /* requested mail action okay, completed */
+            if (NULL == msg) {
+                strcpy(rply_line, "250 OK\r\n");
+                Writen(sockfd, rply_line, strlen(rply_line));
+                return 0;
+            }
+            else {
+                strcpy(rply_line, "250 ");
+                break;
+            }
+
+        case R250E:  /* same as R250, but to be continued */
+            if (NULL == msg) {
+                return -1;  /* message should be user defined */
+            }
+            else {
+                strcpy(rply_line, "250-");
+                break;
+            }
+
+        case R251:  /* (after RCPT) user not local; will forward to... */
+            /* FIXME: <forward-path> should point to next server */
+            if (NULL == msg) {
+                strcpy(rply_line, "251 User not local; "
+                                  "will forward to <forward-path>\r\n");
+                Writen(sockfd, rply_line, strlen(rply_line));
+                return 0;
+            }
+            else {
+                strcpy(rply_line, "251 ");
+                break;
+            }
+
+        case R354:  /* (after DATA) start mail input; end with <CRLF>.<CRLF> */
+            if (NULL == msg) {
+                strcpy(rply_line, "354 Start mail input; "
+                                  "end with <CRLF>.<CRLF>\r\n");
+                Writen(sockfd, rply_line, strlen(rply_line));
+                return 0;
+            }
+            else {
+                strcpy(rply_line, "354 ");
+                break;
+            }
+
+        case R450:  /* requested mail action not taken: mailbox unavailable */
+            if (NULL == msg) {
+                strcpy(rply_line, "450 Requested mail action not taken: "
+                                  "mailbox unavailable\r\n");
+                Writen(sockfd, rply_line, strlen(rply_line));
+                return 0;
+            }
+            else {
+                strcpy(rply_line, "450 ");
+                break;
+            }
+
+        case R451:  /* requested action aborted: local error in processing */
+            if (NULL == msg) {
+                strcpy(rply_line, "451 Requested action aborted: "
+                                  "local error in processing\r\n");
+                Writen(sockfd, rply_line, strlen(rply_line));
+                return 0;
+            }
+            else {
+                strcpy(rply_line, "451 ");
+                break;
+            }
+
+        case R452:  /* requested action not taken:
+                       insufficient system storage */
+            if (NULL == msg) {
+                strcpy(rply_line, "requested action not taken: "
+                                  "insufficient system storage\r\n");
+                Writen(sockfd, rply_line, strlen(rply_line));
+                return 0;
+            }
+            else {
+                strcpy(rply_line, " ");
+                break;
+            }
+
+        case R455:  /* server unable to accommodate parameters */
+            if (NULL == msg) {
+                strcpy(rply_line, "455 Server unable to accommodate "
+                                  "parameters\r\n");
+                Writen(sockfd, rply_line, strlen(rply_line));
+                return 0;
+            }
+            else {
+                strcpy(rply_line, "455  ");
+                break;
+            }
+
+        case R502:  /* (after EHLO) command not implemented */
+            if (NULL == msg) {
+                strcpy(rply_line, "502 Command not implemented\r\n");
+                Writen(sockfd, rply_line, strlen(rply_line));
+                return 0;
+            }
+            else {
+                strcpy(rply_line, "502 ");
+                break;
+            }
+
+        case R503:  /* bad sequence of commands */
+            if (NULL == msg) {
+                strcpy(rply_line, "503 Bad sequence of commands\r\n");
+                Writen(sockfd, rply_line, strlen(rply_line));
+                return 0;
+            }
+            else {
+                strcpy(rply_line, "503 ");
+                break;
+            }
+
+        case R504:  /* (after HELO/EHLO) command parameter not implemented */
+            if (NULL == msg) {
+                strcpy(rply_line, "504 Command parameter not implemented\r\n");
+                Writen(sockfd, rply_line, strlen(rply_line));
+                return 0;
+            }
+            else {
+                strcpy(rply_line, "504 ");
+                break;
+            }
+
+        case R550:  /* requested action not taken: mailbox unavailable */
+            if (NULL == msg) {
+                strcpy(rply_line, "550 Requested action not taken: "
+                                  "mailbox unavailable\r\n");
+                Writen(sockfd, rply_line, strlen(rply_line));
+                return 0;
+            }
+            else {
+                strcpy(rply_line, "550 ");
+                break;
+            }
+
+        case R551:  /* (after RCPT) user not local; please try <forward-path> */
+            /* FIXME: <forward-path> should point to next server */
+            if (NULL == msg) {
+                strcpy(rply_line, "551 User not local; "
+                                  "please try <forward-path>\r\n");
+                Writen(sockfd, rply_line, strlen(rply_line));
+                return 0;
+            }
+            else {
+                strcpy(rply_line, "551 ");
+                break;
+            }
+
+        case R552:  /* requested mail action aborted:
+                       exceeded storage allocation */
+            if (NULL == msg) {
+                strcpy(rply_line, "552 Requested mail action aborted: "
+                                  "exceeded storage allocation\r\n");
+                Writen(sockfd, rply_line, strlen(rply_line));
+                return 0;
+            }
+            else {
+                strcpy(rply_line, "552 ");
+                break;
+            }
+
+        case R553:  /* (after RCPT) requested action not taken:
+                       mailbox name not allowed */
+            if (NULL == msg) {
+                strcpy(rply_line, "553 Requested action not taken: "
+                                  "mailbox name not allowed\r\n");
+                Writen(sockfd, rply_line, strlen(rply_line));
+                return 0;
+            }
+            else {
+                strcpy(rply_line, "553 ");
+                break;
+            }
+
+        case R554:  /* transaction failed */
+            if (NULL == msg) {
+                strcpy(rply_line, "554 Transaction failed\r\n");
+                Writen(sockfd, rply_line, strlen(rply_line));
+                return 0;
+            }
+            else {
+                strcpy(rply_line, "555 ");
+                break;
+            }
+
+        case R555:  /* MAIL FROM/RCPT TO parameters not recognized or
+                       not implemented */
+            if (NULL == msg) {
+                strcpy(rply_line, "555 MAIL FROM/RCPT TO parameters not "
+                                  "recognized or not implemented\r\n");
+                Writen(sockfd, rply_line, strlen(rply_line));
+                return 0;
+            }
+            else {
+                strcpy(rply_line, "555 ");
+                break;
+            }
+
+        default:
+            return -1;  /* no such command */
+            break;
+    }
+
+    /* user defined message */
+    pos = 4;
+    strncpy(rply_line+pos, msg, min(msg_len,LINE_MAXLEN-pos-3));
+    pos += min(LINE_MAXLEN-pos-3, strlen(msg));
+    strcpy(rply_line+pos, "\r\n");
+    Writen(sockfd, rply_line, strlen(rply_line));
 
     return 0;
 }
